df97be2858d2992cf45d2602d41d238aa061cffe
Osoro <Yolight377@mail.ru>
2019-05-04 15:29:36 +0400

    First question

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function anime(name){
-    return n = "Твое любимое аниме " + name;
-}
+let money = +prompt("Ваш бюджет на месяц?");
+let time = +prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
 
-let n = prompt("nan anime ga suki desuka?");
-alert(anime(n));
+let q1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    q2 = prompt("Во сколько обойдется?"),
+    q3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    q4 = prompt("Во сколько обойдется?");
+
+appData.expenses.q1 = q2;
+appData.expenses.q3 = q4;
+
+alert(appData.budget / 30);
